611e8b5b463a71548aa620b0d0d0bb6c8f219ccf
Daniel (Dongdong) Wang <50807233+dwang181@users.noreply.github.com>
2021-12-06 01:04:25 -0500

    Update active_train.sh

--- active_train.sh
+++ active_train.sh
@@ -7,14 +7,14 @@ data=cifar10
 root=./cifar10_data
 model=vgg
 
-model_out=./active_student_models/${data}_${model}_student_model_21000
+model_out=./active_student_models/${data}_${model}_student_model_1000
 
 echo "model_out: " ${model_out}
-CUDA_VISIBLE_DEVICES=1,0,2,3 python3.8 ./active_train.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3.8 ./active_train.py \
                         --lr ${lr} \
                         --data ${data} \
                         --model ${model} \
                         --root ${root} \
                         --model_out ${model_out}.pth \
-                        2>&1 | tee -a ./logs/${data}_${model}_student_model_21000.log
+                        2>&1 | tee -a ./logs/${data}_${model}_student_model_1000.log
 
